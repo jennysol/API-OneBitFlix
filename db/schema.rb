4693147d12cd437e624b58fb73a52e8e41e60ad3
jennysol <jennyssoliver@gmail.com>
2021-08-19 07:40:56 -0300

    Feature
    Adding extra settings for the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_102619) do
+ActiveRecord::Schema.define(version: 2021_08_19_103603) do
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.integer "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
