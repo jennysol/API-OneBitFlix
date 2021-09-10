84472061fa1b21c2a2116f35402fa1530205e57f
jennysol <jennyssoliver@gmail.com>
2021-09-10 07:40:41 -0300

    Update
    Better organization of the movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,19 +1,18 @@
 class Movie < ApplicationRecord
   include PgSearch
-  multisearchable against: [:title], if: lambda{ |record|
-  record.serie. nil ? }
+  multisearchable against: [:title], if: lambda{ |record| record.serie. nil ? }
+  
   belongs_to :serie, optional: true
   belongs_to :category, optional: true
   has_many :reviews, as: :reviewable
   has_many :players, dependent: :destroy
-  has_one :watched_serie, class_name: "Serie", foreign_key:
-  "last_watched_episode_id", dependent: :nullify
+  has_one :watched_serie, class_name: "Serie", foreign_key: "last_watched_episode_id", dependent: :nullify
+
   validates :title, presence: true
   validates :description, presence: true
   validates :thumbnail_key, presence: true
   validates :video_key, presence: true
-  validates :episode_number, presence: true, uniqueness: { scope:
-  :serie_id }, if: ->{ serie.present? }
+  validates :episode_number, presence: true, uniqueness: { scope :serie_id }, if: ->{ serie.present? }
   validates :category, presence: true, if: ->{ serie.nil ? }
   validate :highlight_episode
   
