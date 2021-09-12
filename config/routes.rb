e5e42a055d93783569e25b5e9fccdbc603e5f369
jennysol <jennyssoliver@gmail.com>
2021-09-12 16:01:09 -0300

    Feature
    Adding controller to Recommendations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'recommendations/index'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       get 'executions/create'
       get 'executions/update'
     end
