0d8bad0a7988927d276501c2b7fe6b32ec452613
jennysol <jennyssoliver@gmail.com>
2021-09-12 15:58:18 -0300

    Feature
    Adding controller to Search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'searches/index'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       get 'reviews/index'
       get 'reviews/create'
     end
