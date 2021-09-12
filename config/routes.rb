a374550204b6eb768de43b70d2c6f5e4eb211999
jennysol <jennyssoliver@gmail.com>
2021-09-12 15:57:46 -0300

    Feature
    Adding controller to Review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'reviews/index'
+      get 'reviews/create'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       get 'favorites/index'
       get 'favorites/create'
       get 'favorites/destroy'
