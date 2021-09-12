ce9cd18ef15b914b8697ef5c333a03f89a7dd82f
jennysol <jennyssoliver@gmail.com>
2021-09-12 15:46:31 -0300

    Feature
    Adding controller to Favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'favorites/index'
+      get 'favorites/create'
+      get 'favorites/destroy'
+    end
+  end
   get 'home/index'
   devise_for :users
   root to: "home#index"
