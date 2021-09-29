98a4aa3fb101d31b6e919dc0609e579b9887a539
jennysol <jennyssoliver@gmail.com>
2021-09-29 07:11:56 -0300

    Update
    Updating the serializer movie for aws integration

--- app/serializers/api/v1/movie_serializer.rb
+++ app/serializers/api/v1/movie_serializer.rb
@@ -1,22 +1,43 @@
 class Api::V1::MovieSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :title, :description, :episode_number, thumbnail_key, :featured_thumbnail_key, :video_key, :serie_id
+  attributes :id, :title, :description, :episode_number, :serie_id
   
-  attribute :type do |object|
-    'serie'
-  end
-  
-  attribute :category do |object| 
-    object.category&.name
+  attribute :category do |object|
+   object.category&.name
   end
   
   attribute :reviews_count do |object|
-    object.reviews.count
+   object.reviews.count
   end
   
   attribute :favorite do |object, params|
     if params.present? && params.has_key?(:user)
       params[:user].favorites.where(favoritable: object).exists?
+    end 
+  end
+  
+  attribute :thumbnail_url do |object|
+  
+  AWS_BUCKET.object("thumbnails/#{object.thumbnail_key}").presigned_url
+    (:get, expires_in: 120)
+  end
+  
+  attribute :thumbnail_cover_url do |object|
+  
+  AWS_BUCKET.object("thumbnails/#{object.thumbnail_cover_key}").presign
+    ed_url(:get, expires_in: 120)
+  end
+  
+  attribute :featured_thumbnail_url do |object|
+    if object[:featured_thumbnail_key].present?
+      AWS_BUCKET.object("thumbnails/#{object.featured_thumbnail_key}").pres
+        igned_url(:get, expires_in: 120)
     end
   end
-end
+  
+  attribute :video_url do |object|
+  
+  AWS_BUCKET.object("videos/#{object.video_key}").presigned_url(:get,
+    expires_in: 120)
+  end
+ end
