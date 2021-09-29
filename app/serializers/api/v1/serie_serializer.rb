d82b5f21ffbd5033eea4590997baf2098b2818ee
jennysol <jennyssoliver@gmail.com>
2021-09-29 07:07:57 -0300

    Update
    Updating the serializer for aws integration

--- app/serializers/api/v1/serie_serializer.rb
+++ app/serializers/api/v1/serie_serializer.rb
@@ -1,28 +1,42 @@
 class Api::V1::SerieSerializer
   include FastJsonapi::ObjectSerializer
-
-  attributes :id, :title, :description, thumbnail_key, featured_thumbnail_key
+  attributes :id, :title, :description
   has_many :episodes, record_type: :movies, serializer: :movie
   
-  attribute :type do |object|
-    'serie'
-  end
-  
   attribute :category do |object|
-    object.category.name
+   object.category.name
   end
   
   attribute :last_watched_episode do |object|
-    object.last_watched_episode_id
+   object.last_watched_episode_id
   end
   
   attribute :reviews_count do |object|
-    object.reviews.count
+   object.reviews.count
   end
   
   attribute :favorite do |object, params|
-    if params.present? && params.has_key?(:user)
+   if params.present? && params.has_key?(:user)
     params[:user].favorites.where(favoritable: object).exists?
+   end
+  end
+  
+  attribute :thumbnail_url do |object|
+  
+  AWS_BUCKET.object("thumbnails/#{object.thumbnail_key}").presigned_url
+    (:get, expires_in: 120)
+  end
+  
+  attribute :thumbnail_cover_url do |object|
+  
+  AWS_BUCKET.object("thumbnails/#{object.thumbnail_cover_key}").presign
+    ed_url(:get, expires_in: 120)
+  end
+  
+  attribute :featured_thumbnail_url do |object|
+    if object[:featured_thumbnail_key].present?p
+      AWS_BUCKET.object("thumbnails/#{object.featured_thumbnail_key}").pres
+        igned_url(:get, expires_in: 120)
     end
   end
-end
+ end
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -5,4 +5,4 @@ Aws::Credentials.new(Rails.application.credentials.aws_key,
 Rails.application.credentials.aws_secret)
 })
  
-AWS_BUCKET = Aws::S3::Resource.new.bucket("onebitflix")
+AWS_BUCKET = Aws::S3::Resource.new.bucket("soliverflix")
