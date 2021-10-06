e0a00977332953c5e31cf8d9457cfa281b6dfd23
jennysol <jennyssoliver@gmail.com>
2021-10-05 21:58:02 -0300

    Update
    Update webpack

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::SearchesController < ApplicationController
     search = PgSearch.multisearch(params[:value]).order("created_at DESC")
     render json: Api::V1::WatchableSerializer.new(search.map(&:searchable)).serialized_json
   end
-  
+
   private
-  
+
     def check_search_value
       if params[:value].present? && params[:value].length < 3
         render json: { errors: "Parameter :value must have at least 3 characters" }
--- app/serializers/api/v1/movie_serializer.rb
+++ app/serializers/api/v1/movie_serializer.rb
@@ -1,43 +1,37 @@
 class Api::V1::MovieSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :title, :description, :episode_number, :serie_id
-  
+  belongs_to :serie
+
   attribute :category do |object|
-   object.category&.name
+    object.category&.name
   end
-  
+
   attribute :reviews_count do |object|
-   object.reviews.count
+    object.reviews.count
   end
-  
+
   attribute :favorite do |object, params|
     if params.present? && params.has_key?(:user)
       params[:user].favorites.where(favoritable: object).exists?
-    end 
-  end
-  
-  attribute :thumbnail_url do |object|
-  
-  AWS_BUCKET.object("thumbnails/#{object.thumbnail_key}").presigned_url
-    (:get, expires_in: 120)
-  end
-  
-  attribute :thumbnail_cover_url do |object|
-  
-  AWS_BUCKET.object("thumbnails/#{object.thumbnail_cover_key}").presign
-    ed_url(:get, expires_in: 120)
-  end
-  
-  attribute :featured_thumbnail_url do |object|
-    if object[:featured_thumbnail_key].present?
-      AWS_BUCKET.object("thumbnails/#{object.featured_thumbnail_key}").pres
-        igned_url(:get, expires_in: 120)
     end
   end
-  
-  attribute :video_url do |object|
-  
-  AWS_BUCKET.object("videos/#{object.video_key}").presigned_url(:get,
-    expires_in: 120)
+
+  attribute :thumbnail_url do |object|
+    AWS_BUCKET.object("thumbnails/#{object.thumbnail_key}").presigned_url(:get, expires_in: 120)
   end
- end
+
+  attribute :thumbnail_cover_url do |object|
+    AWS_BUCKET.object("thumbnails/#{object.thumbnail_cover_key}").presigned_url(:get, expires_in: 120)
+  end
+
+  attribute :featured_thumbnail_url do |object|
+    if object[:featured_thumbnail_key].present?
+      AWS_BUCKET.object("thumbnails/#{object.featured_thumbnail_key}").presigned_url(:get, expires_in: 120)
+    end
+  end
+
+  attribute :video_url do |object|
+    AWS_BUCKET.object("videos/#{object.video_key}").presigned_url(:get, expires_in: 120)
+  end
+end
